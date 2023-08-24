f20885b352faa021694e7378803d908041876a17
Habibu Mamudu <habibumamudu@gmail.c>
2023-08-24 10:37:33 -0700

    add nop

---
+++ 17-nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - Doesn't do anything
+ * @stack: Double pointer to the top of the stack
+ * @lin_number: Line number in the file
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
--- monty.h
+++ monty.h
@@ -54,6 +54,8 @@ void _sub(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+
 
 void free_list(stack_t *head);
 void free_tokens(char **tokens);
